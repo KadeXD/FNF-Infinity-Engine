b0285931d3deba8231e80b411d970aa86afcfbb0
LEVIXDDLMAO <levi_xddd@outlook.com>
2022-11-12 19:24:21 +0000

    fixes new icon shit ( SANICBTW FIXED IT

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4075,6 +4075,27 @@ class PlayState extends MusicBeatState
 		if (curBeat % gfSpeed == 0 && !gf.stunned && gf.animation.curAnim.name != null && !gf.animation.curAnim.name.startsWith("sing"))
 		{
 			gf.dance();
+			if (curBeat % gfSpeed == 0) {
+			  curBeat % (gfSpeed * 2) == 0 ? {
+				  iconP1.scale.set(1.1, 0.8);
+				  iconP2.scale.set(1.1, 1.3);
+  
+				  FlxTween.angle(iconP1, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+				  FlxTween.angle(iconP2, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			  } : {
+				  iconP1.scale.set(1.1, 1.3);
+				  iconP2.scale.set(1.1, 0.8);
+  
+				  FlxTween.angle(iconP2, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+				  FlxTween.angle(iconP1, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+			  }
+  
+			  FlxTween.tween(iconP1, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
+			  FlxTween.tween(iconP2, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
+  
+			  iconP1.updateHitbox();
+			  iconP2.updateHitbox();
+			}
 		}
 
 		if(curBeat % 2 == 0) {
@@ -4342,29 +4363,6 @@ class PlayState extends MusicBeatState
 
 		displayRating("miss", "late");
 		popUpCombo();
-		
-		var funny:Float = (healthBar.percent * 0.01) + 0.01;
-		if (curBeat % gfSpeed == 0) {
-			  curBeat % (gfSpeed * 2) == 0 ? {
-				  iconP1.scale.set(1.1, 0.8);
-				  iconP2.scale.set(1.1, 1.3);
-  
-				  FlxTween.angle(iconP1, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
-				  FlxTween.angle(iconP2, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
-			  } : {
-				  iconP1.scale.set(1.1, 1.3);
-				  iconP2.scale.set(1.1, 0.8);
-  
-				  FlxTween.angle(iconP2, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
-				  FlxTween.angle(iconP1, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
-			  }
-  
-			  FlxTween.tween(iconP1, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
-			  FlxTween.tween(iconP2, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
-  
-			  iconP1.updateHitbox();
-			  iconP2.updateHitbox();
-			}
 	}
 	function increaseCombo()
 	{
